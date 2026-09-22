c3a4d08800d685596935d0e20158c9b508b6074f
362688756 <362688756@2260I-L29-009>
2026-09-22 12:16:41 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Topic_5._1___Simple_If_Statements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
